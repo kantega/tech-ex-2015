be51769d4588035207f4400d911ffbb76e8968f5
Kristian Lier Selnæs <kristian.selnaes@kantega.no>
2015-01-27 14:59:56 +0100

    Lagt til modul for dummy backend (for testing)

---
+++ mockbackend/server.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+(function(){
+    var PORT = 9000,
+        express = require("express"),
+        server = express();
+
+    console.log("############################");
+    console.log("Starting server on port " + PORT);
+    console.log("############################");
+
+    var players = {};
+
+    server.listen(PORT);
+
+    server.get("/hello", function(req, res){
+        res.set("Access-Control-Allow-Origin", "*")
+            .json({hello: "I'm OK!"})
+    });
+
+    server.put("/player/:nick", function(req, res) {
+        var nick = req.params.nick;
+        console.log("PUT: /player/"+nick);
+
+        res.set("Access-Control-Allow-Origin", "*");
+        if (players[nick]) {
+            res.status(409)
+                .send({error: "Nick '"+nick+"' already registered"})
+        } else {
+            var playerData = {
+                "quests": ["seealltalks", "networkingchanp"],
+                "preferences": {"drink": "Wine", "eat": "Meat"},
+                "id": generateRandomId(),
+                "nick": nick
+            };
+            players[nick] = playerData;
+            res.status(201)
+                .send(playerData);
+        }
+    });
+
+
+    var generateRandomId = function(){
+        return Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 5);
+    }
+}());
